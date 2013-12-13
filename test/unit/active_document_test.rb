6faab93637733f4bb96df4dbd0ed5234c32321c7
Dmitri Zagidulin <dzagidulin@gmail.com>
2013-12-13 15:21:48 -0500

    Red - Failing tests for ActiveDocument's collection api

---
+++ test/unit/active_document_test.rb
@@ -0,0 +1,36 @@
+## ------------------------------------------------------------------- 
+## 
+## Copyright (c) "2013" Basho Technologies, Inc.
+##
+## This file is provided to you under the Apache License,
+## Version 2.0 (the "License"); you may not use this file
+## except in compliance with the License.  You may obtain
+## a copy of the License at
+##
+##   http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing,
+## software distributed under the License is distributed on an
+## "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+## KIND, either express or implied.  See the License for the
+## specific language governing permissions and limitations
+## under the License.
+##
+## -------------------------------------------------------------------
+
+require 'test_helper'
+
+describe 'a RiakJson::ActiveDocument' do
+  before do
+    @document = RiakJson::ActiveDocument.new
+  end
+  context "should implement the RiakJson Collection API" do
+    it "should have a nil key when first initialized" do
+      @document.key.must_be_nil
+    end
+    
+    it "should respond to to_json_document()" do
+      assert @document.respond_to?(:to_json_document)
+    end
+  end
+end
